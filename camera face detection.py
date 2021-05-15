fe16b58a4b1823a4668d1a0f7af1f4afb84a7df1
RamdasKrishnakumar <kramdas1996@gmail.com>
2021-05-16 06:26:03 +0800

    comments

--- camera face detection.py
+++ camera face detection.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed May 12 02:26:37 2021
-
-@author: LENOVO
-"""
-
 import cv2
 import sys
 
@@ -17,14 +10,14 @@ while True:
     # Capture frame-by-frame
     ret, frame = video_capture.read()
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     faces = faceCascade.detectMultiScale(
-        gray,
+        gray_image,
         scaleFactor=1.1,
         minNeighbors=5,
         minSize=(30, 30),
-        flags=cv2.CASCADE_SCALE_IMAGE
+        flags=cv2.CASCADE_SCALE_IMAGE # for earlier version of opencv this is HAARCASCADE filter 
     )
 
     # Draw a rectangle around the faces
